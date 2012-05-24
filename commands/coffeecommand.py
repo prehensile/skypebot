3ab9f56348080f0770cbe6a23e8344c01860a258
Johannes Kleske <johannes.kleske@gmail.com>
2012-05-24 18:51:02 +0200

    Decent Coffee Setup
    Added Templates, Pre-Modifiers and Coffees, took all the code from the
    tea-command.

--- commands/coffeecommand.py
+++ commands/coffeecommand.py
@@ -5,10 +5,33 @@ class CoffeeCommand(object):
 
 	def __init__(self):
 		
-		self.templates = [ 	Template("stares at $name, disregards the order and serves an home-roasted espresso ristretto.") ]
+		self.templates = [ 	Template("stares at $name, disregards the order and serves an home-roasted espresso ristretto."),
+		 					Template("rolls his eyes, grabs the Aeropress and pours $name $coffee"),
+							Template("looks at $name disgusted and mumbles something about this being a bar and no fucking Starbucks."),
+							Template("smiles, pours $name $coffee and makes it Irish.")]
+		
+		self.pre_modifiers = [ "a steaming cup of",
+				   "some slowly dripped",
+				   "a fine blend of",
+				   "a freshly brewed mug of",
+				   "a shot of",
+				   "a lukewarm mug of preheated"]				
+
+		self.coffees = [ "Espresso",
+					"Nepal Mount Everest Supreme",
+					"Brazil Washed Fazenda da Lagoa",
+					"Ecuador washed Galapagos Isle",
+					"Jamaica Blue Mountain",
+					"Organic Ethiopia Yirgacheffe Adado",
+					"Organic Indonesia Gajah Aceh",
+					"Decaf Santa Ana Espresso"]
+
 		
 	def execute( self, message ):
+		coffee = random.choice( self.coffees )
+		pre_modifier = random.choice( self.pre_modifiers )
+		coffee = "%s %s" % (pre_modifier, coffee)
 		name = message.FromDisplayName
 		template = random.choice( self.templates )
-		message_out = template.substitute(name=name)
+		message_out = template.substitute(name=name, coffee=coffee)
 		return "/me %s" % message_out
