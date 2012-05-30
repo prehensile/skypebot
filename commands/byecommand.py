f876a3b9ce97f6404bd1253f4bb7ee7516908778
MarcusBrown <marcus.brown@booming.de>
2012-05-30 11:00:35 +0300

    a couple of new good byes.

--- commands/byecommand.py
+++ commands/byecommand.py
@@ -10,6 +10,9 @@ class ByeCommand(object):
 		self.templates = [ 	Template("waves goodbye to $name."),
 							Template("throws his hands up as $name finally leaves the bar."),
 							Template("hardly notices that $name leaves."),
+							Template("couldn't give a flying pickle about $name leaving."),
+							Template("breaks wind and blames $name."),
+							Template("calls last orders at the bar."),
 							Template("comes running after $name as they try to dodge the bill, again."),
 							Template("tries to get $name to have one more drink.")
 							]
