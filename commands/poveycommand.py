12801e18cfdfb98a25253955e8821ea7a4d33cdf
MarcusBrown <marcus.brown@booming.de>
2012-05-30 17:00:18 +0300

    some new povey things

--- commands/poveycommand.py
+++ commands/poveycommand.py
@@ -7,7 +7,12 @@ class PoveyCommand(object):
 	def __init__(self):
 		self.templates = [ 	Template("thinks $name completes him."),
 							Template("nurtures a hangover."),
-							Template("writes a blogpost. About METAL."),
+							Template("organises a conference"),
+							Template("makes some badges"),
+							Template("automagically grows a beard"),
+							Template("buggers of to Danemark for a bit."),
+							Template("nips out the back to catch up with the wrestling"),
+							Template("was on the best seat in the bus this morning"),
 							Template("calls $name a divvy"),
 							Template("buggers up a branch.")
 							]
