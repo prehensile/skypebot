905760f436bfeb30efa0470b8a62c1119994a282
Henry Cooke <me@prehensile.co.uk>
2012-05-31 10:57:46 +0200

    added hatcommand

--- commands/commandscratch.py
+++ commands/commandscratch.py
@@ -14,3 +14,27 @@ class ExampleCommand(object):
 		template = random.choice( self.templates )
 		message_out = template.substitute(name=name)
 		return "/me %s" % message_out
+		
+class HatCommand(object):
+
+	def __init__(self):
+		self.templates = [ Template("chucks $name a $hat"),
+				   Template("rummages around in the dressing-up box and produces a battered $hat for $name"),
+				   Template("knocks up a quick $hat for $name using his mad millinery skills.") ]
+		
+		self.hats = ['Akubra', 'Beanie', 'Beret', 'Boater', 'Busby', 'Capuchon', 'Chupalla', 'Deerstalker',
+		'Fedora', 'Kepi', 'Kippah', 'Jew', 'Kufi', 'Nasaq', 'Inuit', 'Pakol', 'Rogatywka', 'Salakot',
+		'Skullcap', 'Sombrero', 'Trilby', 'Tubeteika', 'Tuque', 'Turban', 'Ushanka', 'Zucchetto',
+		'Bicorne', 'Bycoket', 'Capotain', 'Caubeen', 'bicorne', 'Deerstalker', 'Kolpik', 'Peci',
+		'Papakha', 'Shtreimel', 'Spodik', 'Sombrero', 'Pope', 'Mitre', 'Tricorne', 'Trilby',
+		'Capotain', 'Hennin', 'Kokoshnik', 'Vietnam', 'Vietnam', 'Ochipok', 'Tantour', 'Copintank',
+		'Cordies', 'Cossack', 'Crinoline', 'Singapore', 'Jaapi', 'Kausia', 'Kevenhuller', 'Montera',
+		'Mousquetaire', 'Petasos']
+
+	def execute( self, message ):
+		name = message.FromDisplayName
+		template = random.choice( self.templates )
+		hat = random.choice( self.hats )
+		message_out = template.substitute( name=name, hat=hat )
+		return "/me %s" % message_out
+		
