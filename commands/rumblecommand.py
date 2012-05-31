40313cc7e58f0f13c2220764c1c3984523a0ac45
MarcusBrown <marcus.brown@booming.de>
2012-05-31 15:09:15 +0300

    new stuff.

--- commands/rumblecommand.py
+++ commands/rumblecommand.py
@@ -9,12 +9,23 @@ class RumbleCommand(object):
 
 		self.templates = [ 	Template("pounces $name with a $finisher."),
 							Template("performs a $finisher on $name and watches as they tap out."),
+							Template("sets up a $finisher on $name and screams YES YES YES."),
 							Template("tries a $finisher on $name before realising $name really just needs a hug.")
 						 ]
 
 		self.pre_modifiers = [ "sloppy",
 				   "aggressive",
 				   "half-assed attempt of a",
+				   "pathetic",
+				   "must see",
+				   "PPV",
+				   "girly",
+				   "delicate",
+				   "impressive but inaffective",
+				   "smashing",
+				   "crushing",
+				   "horrible",
+				   "buggered up",
 				   "too weak to matter"
 				   ]				
 
@@ -24,6 +35,22 @@ class RumbleCommand(object):
 			"Hulk-Buster",
 			"Russian Sickle",
 			"Mandible Claw",
+			"Big Splash",
+			"Boday Avalanche",
+			"Bronco Buster",
+			"Backhand Chop",
+			"Flying Clothesline",
+			"Butt Drop",
+			"Elbow Drop",
+			"Headbut",
+			"Discus Elbow Smash",
+			"Povey Ponce Breaker",
+			"Bausola Bowel Slam",
+			"Shardcore Elbow Bounce",
+			"Go To Sleep"
+			"Kaiser Wobble Bash",
+			"Snake Eyes",
+			"Mjays Eye Poke",
 			"Steele Trap"]
 
 	def execute( self, message ):
