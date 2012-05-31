71d9b35bfbd6dde1ac5ea0e6f63ebaa826f2ee74
Gregory Povey <gregory.povey@gmail.com>
2012-05-31 14:25:14 +0200

    + many templates.

--- commands/rumblecommand.py
+++ commands/rumblecommand.py
@@ -9,6 +9,21 @@ class RumbleCommand(object):
 
 		self.templates = [ 	Template("pounces $name with a $finisher."),
 							Template("performs a $finisher on $name and watches as they tap out."),
+							Template("wears a flashy dressing grown and shouts 'WOOOOOOO'."),
+							Template("is suspended for 30-days due to a violation of the Wellness Program."),
+							Template("botches a $finisher and $name is stretchered out of the ring."),
+							Template("hollers at Miss Elizabeth."),
+							Template("interferes as !marcus is trying to pin $name."),
+							Template("smashes a steel-chair across $name's back."),
+							Template("challenges $name to a Hell In The Philter Cell match."),
+							Template("is entering the Royal Rumble at number 30."),
+							Template("drops a pipebomb."),
+							Template("takes a mouthful of water and sprays it all over $name."),
+							Template("performs a worked shoot, calls out $name."),
+							Template("takes the snake out of the bag."),
+							Template("waves his Intercontinental Champion belt in $name's face. Oooooh yeeeeah."),
+							Template("is wearing leather chaps."),
+							Template("promotes the core values of Hustle, Loyalty, Respect"),
 							Template("sets up a $finisher on $name and screams YES YES YES."),
 							Template("tries a $finisher on $name before realising $name really just needs a hug.")
 						 ]
@@ -47,7 +62,7 @@ class RumbleCommand(object):
 			"Povey Ponce Breaker",
 			"Bausola Bowel Slam",
 			"Shardcore Elbow Bounce",
-			"Go To Sleep"
+			"Go To Sleep",
 			"Kaiser Wobble Bash",
 			"Snake Eyes",
 			"Mjays Eye Poke",
