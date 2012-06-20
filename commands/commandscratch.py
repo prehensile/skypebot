7d78fcc26ee29171e7497c0a10914246132c0915
Gregory Povey <gregory.povey@gmail.com>
2012-06-20 14:26:15 +0200

    + adman

--- commands/commandscratch.py
+++ commands/commandscratch.py
@@ -26,3 +26,22 @@ class KnockKnockCommand( commandbase.BaseCommand ):
 		message_out = template.substitute(name=name)
 		return "/me %s" % message_out
 		
+		
+class AdmanCommand( commandbase.BaseCommand ):
+
+	def __init__(self):
+		self.templates = [ 	Template("does a brand onion for $name."),
+					Template("leverages key influencers."),
+					Template("makes it count."),
+					Template("is in Cannes."),
+					Template("delivers an insight to $name."),
+					Template("is making a deck."),
+					Template("is a T-shaped person."),
+					Template("strategises."),
+					Template("considers what $name's metrics are.") ]
+
+	def generate( self, name ):
+		template = random.choice( self.templates )
+		message_out = template.substitute(name=name)
+		return "/me %s" % message_out
+
