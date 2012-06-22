102b6891986d0ae46584b130de0d9e011b3665e5
Henry Cooke <me@prehensile.co.uk>
2012-06-22 13:17:23 +0100

    enabled Skype & twitter

--- skypebot.py
+++ skypebot.py
@@ -31,8 +31,8 @@ class ChatHandler(object):
                 #print message.Id
         return new_messages
 
-RUN_SKYPE = False
-ENABLE_TWITTER = False
+RUN_SKYPE = True
+ENABLE_TWITTER = True
 ENABLE_GIFTS = True
 class BotThread( queuedthread.QueuedThread ):
     
