4d566d07994a83ff361839b27e7cd46c6cbe24d4
Henry Cooke <me@prehensile.co.uk>
2012-07-03 23:02:43 +0100

    more testing

--- commands/commandscratch.py
+++ commands/commandscratch.py
@@ -24,7 +24,7 @@ class TestCommand( BaseCommand ):
     def __init__(self):
         BaseCommand.__init__( self )
         self.command_mappings = [ "test" ]
-        self.templates = [  Template("test2") ]
+        self.templates = [  Template("test3") ]
 
     def generate( self, name ):
         template = random.choice( self.templates )
