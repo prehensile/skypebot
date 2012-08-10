079f6ce46e86d9b21ded71ab456c320ffde0603a
MarcusBrown <marcus.brown@booming.de>
2012-08-10 13:09:02 +0300

    Update commands/commandscratch.py
    shardcore command

--- commands/commandscratch.py
+++ commands/commandscratch.py
@@ -50,6 +50,26 @@ class TinyCommand( BaseCommand ):
         message_out = template.substitute(name=name)
         return "/me %s" % message_out
         
+        
+class ShardcoreCommand( BaseCommand ):
+
+    def __init__(self):
+        BaseCommand.__init__( self )
+        self.command_mappings = [ "shardcore" ]
+        self.templates = [  Template("paints a lovely painting of Morrissey."),
+                            Template("is told to bite his tongue by $name."),
+                            Template("is denied access to the United States of Botstep."),
+                            Template("is disappointed by all the swearing."),
+                            Template("just wants to finger paint."),
+                            Template("has declared world domination of techno sleaze has his ultimate goal."),
+                            Template("has a seriously chat with the cleaning lady.") ]
+
+    def generate( self, name ):
+        template = random.choice( self.templates )
+        message_out = template.substitute(name=name)
+        return "/me %s" % message_out
+        
+        
 
 class LoveCommand( BaseCommand ):
 
