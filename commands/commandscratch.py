dec5c772d28f86da9c2a70a52195d4471a545b6c
shardcore <shardcore@shardcore.org>
2012-08-10 09:56:20 +0100

    added some stuff to art, tiny and love

--- commands/commandscratch.py
+++ commands/commandscratch.py
@@ -32,8 +32,17 @@ class TinyCommand( BaseCommand ):
                             Template("appears to have been in some very cold water."),
                             Template("is all tilt-shifted and that."),
                             Template("is wearing a size 4 shoe."),
-                            Template("declares that size doesn\'t matter as long as it s tiny"),
+                            Template("declares that size doesn\'t matter as long as it\'s tiny"),
                             Template("shrinks $name and injects them into the blood stream of !satan."),
+                            Template("is worried about quantum effects"),
+                            Template("is swept up in a tsunami of brownian motion"),
+                            Template("is the smallest human being, a microscopic man, he can do all the things the micro-man can"),
+                            Template("is swept up in a tsunami of brownian motion"),
+                            Template("shrinks $name in the hot wash"),
+                            Template("rides in on a flea"),
+                            Template("hides from the big kids on the big web"),
+                            Template("takes $name into a secret room, even tinier than this one"),
+                            Template("complains that this matchbox is too roomy"),
                             Template("dances to Ant Music.") ]
 
     def generate( self, name ):
@@ -62,6 +71,11 @@ class LoveCommand( BaseCommand ):
                             Template("puts on a pair of suspenders for $name."), 
                             Template("breaks into $name's house and leaves a box of Milk Tray."), 
                             Template("hits up $name for a booty call."), 
+                            Template("does a sexy fan dance for $name"),
+                            Template("is wearing a peephole bra"),
+                            Template("writes a sonnet"),   
+                            Template("simply says \'$name, I love you\' "),
+                            Template("shoots cupid\'s arrow at $name"),  
                             Template("asks $name out for a !drink.") ]
 
     def generate( self, name ):
@@ -113,6 +127,21 @@ class ArtCommand( BaseCommand ):
                             Template("works part-time in a bar to pay his rent."),
                             Template("makes a plaster cast of $name's arse."),
                             Template("sells some old shit to a posh twat."),
+                            Template("paints a lovely picture of $name then sets it on fire"),
+                            Template("wonders why he's so poor"),
+                            Template("strops off saying \'you just don\'t understand me\!\'"),
+                            Template("makes a preparatory sketch"),
+                            Template("asks you to take all your clothes off and lie over there"),
+                            Template("cuts off his ear and posts it to pambot"),
+                            Template("drinks too much absinthe and gets punchy"),
+                            Template("wears a smock"),
+                            Template("paints it black"),
+                            Template("makes some street art, yeah\?\!"),
+                            Template("tries to work out what to rip off next"),
+                            Template("cleans his brushes"),
+                            Template("loses an eye in a duel"),
+                            Template("abandons his family and moves to a tropical island"),
+                            Template("exhibits at the white cube"),
                             Template("invites you to his PV.") ]
 
     def generate( self, name ):
