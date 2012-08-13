aff1203ec1ccb79e28bc5b37d288ce2e31ded86d
zeroinfluencer <zeroinfluencer@philterphactory.com>
2012-08-13 15:52:55 +0100

    more aces

--- commands/aces.py
+++ commands/aces.py
@@ -10,9 +10,22 @@ class HaiCommand( BaseCommand ):
 		BaseCommand.__init__( self )
 		self.command_mappings = [ "aces" ]
 		self.templates = [ 	Template("tuts and nods at $name"),
-							Template("points out to $name that 5 aces is suspect ."),
-							Template("passes $name a !drink"),
-							Template("marks it up on the board. deux points for $name")
+							Template("points out to $name that 5 aces is suspect."),
+							Template("passes $name a !drink."),
+							Template("calls $name a !cock."),
+							Template("throws a dolphin at $name."),
+							Template("!ponders and calls $name's bluff with a full house."),
+							Template("calls !bullshit on $name and dishes out the !cheese."),
+							Template("grins and performs some tiny !magic for $name."),
+							Template("chokes."),
+							Template("disagrees."),
+							Template("reopens the bubbly wine."),
+							Template("looks concerned."),
+							Template("instagrams the moment."),
+							Template("shares the moment on Path."),
+							Template("writes another rule in the tiny web manifesto."),
+							Template("adds scampi to the dinner menu."),
+							Template("marks it up on the board. deux points for $name.")
 							]
 							
 	def generate( self, name ):
